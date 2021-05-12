64b09a08f5b707339e448c3f303f395c01936c66
unknown <DMT1709533@xmu.edu.my>
2021-05-12 14:42:12 +0800

    Implement transfer

--- ERC20.sol
+++ ERC20.sol
@@ -81,4 +81,10 @@ contract NewToken2 is ERC20Interface, SafeMath {
         emit Approval(msg.sender, spender, tokens);
         return true;
     }
+    function transfer(address to, uint tokens) public returns (bool success) {
+        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
 }
